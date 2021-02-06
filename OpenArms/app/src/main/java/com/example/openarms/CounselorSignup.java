790d2d95439ebb4a51a2a7ccf1173cc1b570c6e1
KianSalehi <kian.k.salehi@gmail.com>
2021-02-05 21:00:37 -0500

    CounselorSignup and StudentSignup added, Firebase integration added

--- OpenArms/app/src/main/java/com/example/openarms/CounselorSignup.java
+++ OpenArms/app/src/main/java/com/example/openarms/CounselorSignup.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class StudentLogin extends AppCompatActivity {
+public class CounselorSignup extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_student_login);
+        setContentView(R.layout.activity_counselor_signup);
     }
 }
