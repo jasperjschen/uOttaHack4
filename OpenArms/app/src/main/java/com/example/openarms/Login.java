6bf30712eb5df2b6723e73b54f4cd14036ed6ab9
KianSalehi <kian.k.salehi@gmail.com>
2021-02-06 00:33:30 -0500

    StudentSignup and Login are updated

--- OpenArms/app/src/main/java/com/example/openarms/Login.java
+++ OpenArms/app/src/main/java/com/example/openarms/Login.java
@@ -6,40 +6,68 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 public class Login extends AppCompatActivity {
     private FirebaseAuth mAuth;
-    private String userType;
-    private String email;
-    private String password;
+    String userType;
+    private String userEmail;
+    private String userPassword;
     private final String TAG = "Login: ";
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         mAuth=FirebaseAuth.getInstance();
+        TextView email = findViewById(R.id.loginEmailInput);
+        TextView password = findViewById(R.id.loginPasswordInput);
+        Button logIn = findViewById(R.id.loginButton);
+        userEmail = email.getText().toString();
+        userPassword = password.getText().toString();
 
-        //Function to Signin
-        mAuth.signInWithEmailAndPassword(email,password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()){
-                            Log.d(TAG,"signInWithCustomEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            // Add the functionality to check in firestore if the user is a student or a counselor
-                            //userType =
-                            updateUI(user);
-                        }
-                    }
-                });
+        //Function to Signin and on click listener for logIn button
+        logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signInWithEmailAndPassword(userEmail,userPassword)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()){
+                                    Log.d(TAG,"signInWithCustomEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    CollectionReference students = db.collection("students");
+                                    CollectionReference counselors = db.collection("counselors");
+                                    Query studentQuery = students.whereEqualTo("ID", user.getUid());
+                                    Query counselorQuery = counselors.whereEqualTo("ID",user.getUid());
+                                    if (studentQuery != null){
+                                        userType="student";
+                                        updateUI();
+                                    }else if (counselorQuery !=null){
+                                        userType="counselor";
+                                        updateUI();
+                                    }else{
+                                        Toast.makeText(Login.this, "Could not find you as a student or counselor", Toast.LENGTH_LONG);
+                                    }
+                                }
+                            }
+                        });
+            }
+        });
+
     }
 
 
@@ -48,10 +76,10 @@ public class Login extends AppCompatActivity {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        updateUI(currentUser);
+        updateUI();
     }
 
-    public void updateUI(FirebaseUser user){
+    public void updateUI(){
         if (userType.equals("student")){
             startActivity(new Intent(this, StudentHomePage.class));
         } else{
--- OpenArms/app/src/main/java/com/example/openarms/StudentSignup.java
+++ OpenArms/app/src/main/java/com/example/openarms/StudentSignup.java
@@ -3,41 +3,81 @@ package com.example.openarms;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class StudentSignup extends AppCompatActivity {
     private FirebaseAuth mAuth;
-
+    Map<String, Object> userInfo = new HashMap<>();
+    String userEmail;
+    String userPass;
+    String userName;
+    private final String TAG= "StudentSignup";
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_signup);
         mAuth = FirebaseAuth.getInstance();
+        TextView email = findViewById(R.id.studentSignupEmailTextView);
+        TextView pass = findViewById(R.id.studentSignupPasswordTextView);
+        TextView name = findViewById(R.id.studentSignUpNameTextView);
+        Button signUp = findViewById(R.id.studentSignUpButton);
+        userEmail=email.getText().toString();
+        userName = name.getText().toString();
+        userPass = pass.getText().toString();
 
-
-
+        // On Click listener for sign up button and the sign up function for firebase
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.createUserWithEmailAndPassword(userEmail, userPass)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()){
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "createUserWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                            .setDisplayName(userName).build();
+                                    user.updateProfile(profileUpdates);
+                                    userInfo.put("Name", user.getDisplayName());
+                                    userInfo.put("Email", user.getEmail());
+                                    userInfo.put("ID", user.getUid());
+                                    db.collection("students").document(user.getUid())
+                                            .set(userInfo);
+                                    updateUI(user);
+                                }
+                            }
+                        });
+            }
+        });
         //function to signup
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()){
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "createUserWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user, name);
-                        }
-                    }
-                });
-    }
+            }
     public void updateUI(FirebaseUser user){
-
+        if (user != null) {
+            Toast.makeText(this, "You Signed In successfully!", Toast.LENGTH_LONG).show();
+            startActivity(new Intent(this, StudentHomePage.class));
+        }else{
+            Toast.makeText(this, "Something went wrong please check your email and password", Toast.LENGTH_SHORT).show();
+        }
     }
 }
