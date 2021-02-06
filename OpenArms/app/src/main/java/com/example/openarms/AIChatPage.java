1e04082a43016a5cada22d97e1a295f03ebd09b5
KianSalehi <kian.k.salehi@gmail.com>
2021-02-06 01:42:13 -0500

    New pages added

---
+++ OpenArms/app/src/main/java/com/example/openarms/AIChatPage.java
@@ -0,0 +1,14 @@
+package com.example.openarms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AIChatPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a_i_chat_page);
+    }
+}
---
+++ OpenArms/app/src/main/java/com/example/openarms/CounselorStudentChatPage.java
@@ -0,0 +1,14 @@
+package com.example.openarms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CounselorStudentChatPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_counselor_student_chat_page);
+    }
+}
