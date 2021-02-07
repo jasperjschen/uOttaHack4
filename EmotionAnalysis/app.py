33f5ee971ad51eca3361eb7ae29a71a8e4562569
Jasper <jasperjs.chen@mail.utoronto.ca>
2021-02-07 00:59:48 -0500

    allowed android to communicate w/ flask

--- EmotionAnalysis/app.py
+++ EmotionAnalysis/app.py
@@ -4,13 +4,18 @@ import model as util
 import numpy as np
 import os
 
+
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 app = Flask(__name__)
 
 model = keras.models.load_model("resources/model2.h5")
 
-messages = {"0": "Stress message",
-            "1": "Sad message"}
+messages = {"0": "It's okay! Everybody gets stressed sometimes. I always find meditation, exercise, and/or "
+                 "\"unplugging\" from electronics to be helpful.",
+            "1": "Sometimes, things get hard. Whenever you find yourself doubting how far you can go, "
+                 "just remember how far you have come. "
+                 "Remember everything you have faced, all the battles you have won, "
+                 "and all the fears you have overcome."}
 
 
 @app.route('/predict', methods=["POST"])
@@ -32,15 +37,18 @@ def predict():
             print("Prediction: ", np.argmax(prediction), "Confidence: ", prediction[np.argmax(prediction)] * 100)
             print(prediction * 100)
 
-            if prediction[np.argmax(prediction)] > 0.78:
+            if prediction[np.argmax(prediction)] > 0.75:
                 return jsonify(prediction=str(np.argmax(prediction)),
                                confidence=str(prediction[np.argmax(prediction)]),
                                message=messages[str(np.argmax(prediction))])
             else:
                 return jsonify(prediction=str(np.argmax(prediction)),
                                confidence=str(prediction[np.argmax(prediction)]),
-                               message="NONE")
+                               message="Unfortunately, I am not advanced enough to understand your message. "
+                                       "However, feel free to continue messaging me! "
+                                       "Even if I don't understand your message, "
+                                       "I will make sure to listen to your worries. :)")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False, host="0.0.0.0", port=5000)
